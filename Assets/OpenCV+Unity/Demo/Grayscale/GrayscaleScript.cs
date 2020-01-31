ba0c8c72c777eafc3a0ff5a637f24368b0ae8664
oscfri <oscar.c.friberg@gmail.com>
2020-01-31 23:03:05 +0100

    Added Opencv

---
+++ Assets/OpenCV+Unity/Demo/Grayscale/GrayscaleScript.cs
@@ -0,0 +1,43 @@
+﻿namespace OpenCvSharp.Demo
+{
+	using UnityEngine;
+	using System.Collections;
+	using OpenCvSharp;
+	using UnityEngine.UI;
+
+	public class GrayscaleScript : MonoBehaviour {
+
+		public Texture2D texture;
+
+		// Use this for initialization
+		void Start () {
+
+			Mat mat = Unity.TextureToMat (this.texture);
+			Mat grayMat = new Mat ();
+			Cv2.CvtColor (mat, grayMat, ColorConversionCodes.BGR2GRAY);
+
+			CLAHE clahe = CLAHE.Create();
+			clahe.Apply(grayMat, grayMat);
+
+			Mat grayMat32F = new Mat();
+
+			grayMat.ConvertTo(grayMat32F, MatType.CV_32F);
+			grayMat32F *= 0.5f;
+			grayMat32F.ConvertTo(grayMat, MatType.CV_8U);
+
+			Texture2D texture = Unity.MatToTexture (grayMat);
+
+			RawImage rawImage = gameObject.GetComponent<RawImage> ();
+			rawImage.texture = texture;
+	//		Renderer renderer = gameObject.GetComponent<Renderer> ();
+	//		renderer.material.mainTexture = texture;
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+
+
+	}
+}
---
+++ Assets/_Game/Scripts/GateSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateSpawner : MonoBehaviour
+{
+    public GameObject Gate;
+    public float BeatsPerMinute;
+
+    float timeToBeat = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToBeat -= Time.deltaTime;
+        if (timeToBeat <= 0.0f)
+        {
+            SpawnGate();
+            ResetTimer();
+        }
+    }
+
+    private void SpawnGate()
+    {
+        Instantiate(Gate, transform);
+    }
+
+    private void ResetTimer()
+    {
+        timeToBeat += 60.0f / BeatsPerMinute;
+    }
+}
