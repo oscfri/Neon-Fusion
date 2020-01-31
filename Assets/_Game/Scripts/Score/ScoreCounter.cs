b0f2ded220074055508fa4a1b4203a00fd799ced
David Zetterdahl <david.zetterdahl@hiq.se>
2020-01-31 23:56:58 +0100

    Added a score counter.

---
+++ Assets/_Game/Scripts/Score/ScoreCounter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public float Score = 0;
+
+    private float scorePerHit = 1f;
+    private float scorePerMultiplier = 10f;
+
+    private float multiplier = 1f;
+    private float multiplierAdd = 0.1f;
+
+
+    public void AddScore(Direction direction)
+    {
+        Score += GetScore(direction);
+    }
+
+    private void IncreaseMultiplier()
+    {
+        multiplier += multiplierAdd;
+    }
+
+    public void Reset()
+    {
+        multiplier = 1f;
+        Score = 0;
+    }
+
+    private float GetScore(Direction direction)
+    {
+        if (direction == Direction.special)
+        {
+            IncreaseMultiplier();
+            return multiplierAdd * multiplier;
+        }
+        return scorePerHit * multiplier;
+    }
+}
