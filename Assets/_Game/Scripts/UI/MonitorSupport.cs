b627743220eacf6c36a502631fd0416ac0971c25
David Zetterdahl <david.zetterdahl@hiq.se>
2020-02-01 21:06:49 +0100

    FIX THE DAMN FSHIET

--- Assets/_Game/Scripts/UI/MonitorSupport.cs
+++ Assets/_Game/Scripts/UI/MonitorSupport.cs
@@ -6,16 +6,13 @@ public class MonitorSupport : MonoBehaviour
 
     void Start()
     {
-        void Start()
-        {
-            Debug.Log("displays connected: " + Display.displays.Length);
-            // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
-            // Check if additional displays are available and activate each.
+        Debug.Log("displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
+        // Check if additional displays are available and activate each.
 
-            for (int i = 1; i < Display.displays.Length; i++)
-            {
-                Display.displays[i].Activate();
-            }
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
         }
     }
 
