9f3ee0f448a6106d1efb26487f2c293a9ee35df6
David Zetterdahl <david.zetterdahl@hiq.se>
2020-02-01 17:33:29 +0100

    Tweaked scoring collision and added UI for scoring.

---
+++ Assets/_Game/Scripts/UI/UIScoreText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScoreText : MonoBehaviour
+{
+    private Text text;
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetScore(float score, float multiplier)
+    {
+        text.text = score + " - x" + multiplier;
+    }
+}
