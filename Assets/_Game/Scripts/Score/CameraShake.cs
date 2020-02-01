20d39a7b057b794a0b5d128b97bbbc8756b9ec9f
David Zetterdahl <david.zetterdahl@hiq.se>
2020-02-01 17:07:44 +0100

    Added camera shake when scoring for that player.

---
+++ Assets/_Game/Scripts/Score/CameraShake.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	private Transform camTransform;
+
+	// How long the object should shake for.
+	private float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	private float shakeAmount = 0.015f;
+	private float decreaseFactor = 1.0f;
+	private float timePerShake = 0.35f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	public void Shake()
+	{
+		shakeDuration = timePerShake;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+}
