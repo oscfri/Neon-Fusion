e81ef4b751dc1829f78bf055a43d4b6d5b133890
oscfri <oscar.c.friberg@gmail.com>
2020-02-01 14:30:17 +0100

    Fixed a parameter thing attached to the beat

---
+++ Assets/_Game/Scripts/Visual/Jumper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    private VisualBeat visualBeat;
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visualBeat = GetComponent<VisualBeat>();
+        originalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = originalPosition + transform.up * visualBeat.Value;
+    }
+}
