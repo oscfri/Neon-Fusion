2702ad49a38c5cfb2fc62d0b899520885e68b31a
David Zetterdahl <david.zetterdahl@hiq.se>
2020-02-01 21:41:24 +0100

    Added a scene loader to reload, reload with enter.

---
+++ Assets/_Game/Scripts/Gameplay/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private bool hasReloaded = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasReloaded && Input.GetButtonDown("Submit"))
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        hasReloaded = true;
+        SceneManager.LoadScene("dz");
+    }
+}
