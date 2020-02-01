842bc98714a5b11996db7ba607dd97127a614ff1
oscfri <oscar.c.friberg@gmail.com>
2020-02-01 15:15:32 +0100

    Added emission beat

---
+++ Assets/_Game/Scripts/Visual/Emission.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emission : MonoBehaviour
+{
+    public string ColorId;
+
+    private VisualBeat visualBeat;
+    private Renderer rend;
+    private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visualBeat = GetComponent<VisualBeat>();
+        rend = GetComponent<Renderer>();
+        originalColor = rend.material.GetColor(ColorId);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color interpolated = Color.Lerp(originalColor, Color.white, visualBeat.Value);
+        rend.material.SetColor(ColorId, interpolated);
+    }
+}
